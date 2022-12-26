19ca9e4af242782491e52e20f131597b0893f6e0
Ashu24patil <patilashwini566@gmail.com>
2022-12-27 02:17:26 +0530

    Done by Ashu

--- src/main/java/com/teampractice/TeampracticeApplication.java
+++ src/main/java/com/teampractice/TeampracticeApplication.java
@@ -2,12 +2,27 @@ package com.teampractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.teampractice.model.User;
+import com.teampractice.service.UserServiceImpl;
 
 @SpringBootApplication
 public class TeampracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TeampracticeApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(TeampracticeApplication.class, args);
+	
+		UserServiceImpl bean = context.getBean(UserServiceImpl.class);
+		
+		User u1 = new User();
+		u1.setId(1);
+		u1.setName("Ashwini");
+		u1.setPassword("123");
+		u1.setAge(27);
+		
+		bean.Saveuser(u1);
+	
 	}
 
 }
